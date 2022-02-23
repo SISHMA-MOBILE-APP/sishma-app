999ab30585e505c45c618f5e46a3ee44159c1d96
Shivam Akhouri <shivamakhouri1142@gmail.com>
2022-02-23 08:50:43 +0530

    color codes added

---
+++ utils/colors.js
@@ -0,0 +1,27 @@
+module.exports={
+    color:{
+        "white": "#ffffff",
+        "greenTint10": "#e7f3ed",
+        "greenTint20" : "#d0e8db",
+        "greenTint30": "#b8dcc8",
+        "greenTint40" : "#a0d0b6",
+        "greenTint50": "#89c5a4",
+        "greenTint60": "#71b992",
+        "greenTint70": "#59ad80",
+        "greenTint80" : "#41a16d",
+        "greenTint90": "#2a965b",
+        "greenTint100": "#128a49",
+        "greenShade0": "#128a49",
+        "greenShade10": "#107c42",
+        "greenShade20": "#0e6e3a",
+        "greenShade30" : "#0d6133",
+        "greenShade40" : "#0b532c",
+        "greenShade50": "#094525",
+        "greenShade60": "#07371d",
+        "greenShade70": "#052916",
+        "greenShade80" : "#041c0f",
+        "greenShade90": "#020e07",
+        "black": "#000000",
+        "luminateGreen": "#1ddf76"
+    }
+}
