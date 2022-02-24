f5457cbcd258ffe9eda86a64943152ed8d2b7984
Shreyansh-Agrawal <shreyansh.brbd@gmail.com>
2022-02-24 14:09:45 +0530

    Modified: CustomTextField/index.js

--- components/CustomTextField/index.js
+++ components/CustomTextField/index.js
@@ -8,7 +8,7 @@ export default function InputText(props) {
       <TextInput
         style={styles.inputText}
         placeholder={props.placeholderText}
-        placeholderTextColor="#a0d0b6"
+        placeholderTextColor="#808080"
         secureTextEntry={props.visibility}
         onChangeText={(newText) => setText(newText)}
         defaultValue={text}
@@ -20,21 +20,24 @@ export default function InputText(props) {
 const styles = StyleSheet.create({
   inputView: {
     width: "100%",
-    height: 70,
+    height: 60,
     flexDirection: "row",
     alignSelf: "center",
-    borderWidth: 5,
-    elevation: 1,
-    borderRadius: 35,
-    borderColor: "#a0d0b6",
-    justifyContent: "space-around",
-    backgroundColor: "#e7f3ed",
+    elevation:0,
+    borderRadius: 2,
+    borderBottomWidth:1,
+    borderBottomColor:'rgba(54, 69, 79, 0.5)' ,
+    backgroundColor: "#d0e8db",
     alignItems: "center",
     marginBottom: 6,
   },
   inputText: {
     fontSize: 18,
     alignSelf: "center",
-    color: "#41a16d",
+    color: "#36454F",
+    width:'100%',
+    height:'100%',
+    opacity:0.8,
+    letterSpacing:1
   },
 });
