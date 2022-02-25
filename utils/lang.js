4f40b5e116a62d0e04d601f292539bdd072bde7b
Shreyansh-Agrawal <shreyansh.brbd@gmail.com>
2022-02-25 10:53:44 +0530

    Added: utils/lang.js

---
+++ utils/lang.js
@@ -0,0 +1,86 @@
+module.exports={
+    english:{
+        /* Admin Dashboard */
+        signIn: "Sign In",
+        registerNow: "Register Now",
+        adminDash: "Admin Dashboard",
+        agriFieldOffDB: "Agricultural field officers database",
+        farmersDB: "Farmers database",
+        agriActDB: "Agricultural activities database",
+        loginDash: "Login Dashboard",
+        aadhaarnum: "Aadhar number",
+        otp: "OTP",
+
+        /* Farmer Dashboard */
+        regDash: "Registration dashboard",
+        name: "Name",
+        residenAddress: "Residential address",
+        village: "Village",
+        subDist: "Sub District",
+        district: "District",
+        pin: "PIN",
+        mobileNum: "Mobile Number",
+        sishmaKitNo : "Sishma kit no",
+
+        /* Registration Dashboard */
+        empCode: "Employee Code",
+        designation: "Designation",
+        OffAddress: "Official address",
+    },
+    hindi:{
+        /* Admin Dashboard */
+        signIn: "साइन इन करें",
+        registerNow: "अभी पंजीकरण करें",
+        adminDash: "व्यवस्थापक डैशबोर्ड",
+        agriFieldOffDB: "कृषि क्षेत्र अधिकारी डेटाबेस",
+        farmersDB: "किसान डेटाबेस",
+        agriActDB: "कृषि गतिविधियों डेटाबेस",
+        loginDash: "लॉगिन डैशबोर्ड",
+        aadhaarnum: "आधार नंबर",
+        otp: "OTP",
+
+        /* Farmer Dashboard */
+        regDash: "पंजीकरण डैशबोर्ड",
+        name: "नाम",
+        residenAddress: "आवासीय पता",
+        village: "गाँव",
+        subDist: "उप जिला",
+        district: "जिला",
+        pin: "PIN",
+        mobileNum: "मोबाइल नंबर",
+        sishmaKitNo : "सिशमा किट नंबर",
+
+        /* Registration Dashboard */
+        empCode: "कर्मचारी कोड",
+        designation: "पद",
+        OffAddress: "आधिकारिक पता",
+    },
+    tamil:{
+        /* Admin Dashboard */
+        signIn: "உள்நுழைக",
+        registerNow: "இப்போது பதிவு செய்யவும்",
+        adminDash: "நிர்வாக டாஷ்ப ோர்ட",
+        agriFieldOffDB: "வேளாண் துறை அலுவலர்கள் தரவுத்தளம",
+        farmersDB: "விவசாயிகள் தரவுத்தளம",
+        agriActDB: "விவசாய நடவடிக்கைகள் தரவுத்தளம",
+        loginDash: "உள்நுழைவு டாஷ்போர்டு",
+        aadhaarnum: "ஆதார் எண்",
+        otp: "OTP",
+
+        /* Farmer Dashboard */
+        regDash: "பதிவு டேஷ்போர்டு",
+        name: "பெயர்",
+        residenAddress: "வீட்டு முகவரி",
+        village: "கிராமம்",
+        subDist: "துணைமாவட்டம்",
+        district: "மாவட்டம்",
+        pin: "PIN",
+        mobileNum: "கைபேசி எண்",
+        sishmaKitNo : "சிஷ்மா கிட் எண்",
+
+        /* Registration Dashboard */
+        empCode: "பணியாளர் குறியீடு",
+        designation: "பதவி",
+        OffAddress: "அதிகாரப்பூர்வ முகவரி",
+    }
+}
