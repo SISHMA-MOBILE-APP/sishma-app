4db4a356021bf3f484f818532c427a84a0bc8636
Shreyansh-Agrawal <shreyansh.brbd@gmail.com>
2022-05-21 14:03:06 +0530

    FO recommendation: added transcription

--- pages/FieldOfficerRecommendation/index.js
+++ pages/FieldOfficerRecommendation/index.js
@@ -1,64 +1,68 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { View, Text, StyleSheet, Image, ScrollView} from 'react-native';
 import Card from '../../components/Card';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+// Language Provider
+import { Language } from "../../providers/languageProvider";
+import { transcription } from "../../utils/lang";
 
 export default function FieldOfficerRecommendation(props){
+    const lang = useContext(Language);
     return(
         <View style={styles.container}>
 
         <ScrollView contentContainerStyle={[styles.container, {alignItems: "center"}]}>
-            <Card heading="Recommended Crop :" image={<Image source={require("../../assets/wheat-sack.png")} style={{width: 60, height: 60, borderRadius: 5, marginRight:"10%"}} />}>
-                <Text style={{color: "#0e6e3a", fontSize: 30}}>Wheat</Text>
+            <Card heading={transcription[lang.language]["recommendCrop"]}>
+                <Text style={{color: "#0e6e3a", fontSize: 30}}>Wheat {<Image source={require("../../assets/wheat-sack.png")} style={{width: 35, height: 35, borderRadius: 5, marginRight:"10%"}} />}</Text>
             </Card>
-            <Card heading="Fertilizer Combination 1 :">
+            <Card heading={transcription[lang.language]["fertilizerComb"]+"1 :"}>
                 <View style={{flexDirection: "row", justifyContent: "space-around", width: "95%", marginTop: 15}}>
                     <View style={{alignItems: "center"}}>
-                        <Text>Nitrogen</Text>
+                        <Text>{transcription[lang.language]["nitrogen"]}</Text>
                         <Image source={require("../../assets/nitrogen.png")} style={{width: 50, height: 50, borderRadius: 0, marginTop: 5, marginBottom: 10}} />
                         <Text style={{color: "green",fontSize: 25}}>44.5%</Text>
                     </View>
                     <View style={{width: 0.5, height :"85%", backgroundColor: "black"}} />
                     <View style={{alignItems: "center"}}>
-                        <Text>Phosphorus</Text>
+                        <Text>{transcription[lang.language]["phosphorus"]}</Text>
                         <Image source={require("../../assets/phosphorus.png")} style={{width: 50, height: 50, borderRadius: 0, marginTop: 5, marginBottom: 10}} />
                         <Text style={{color: "green",fontSize: 25}}>30.0%</Text>
                     </View>
                     <View style={{width: 0.5, height :"85%", backgroundColor: "black"}} />
                     <View style={{alignItems: "center"}}>
-                        <Text>Potassium</Text>
+                        <Text>{transcription[lang.language]["potassium"]}</Text>
                         <Image source={require("../../assets/potassium.png")} style={{width: 50, height: 50, borderRadius: 0, marginTop: 5, marginBottom: 10}} />
                         <Text style={{color: "green",fontSize: 25}}>25.5%</Text>
                     </View>
                 </View>
             </Card>
 
-            <Card heading="Fertilizer Combination 2 :">
+            <Card heading={transcription[lang.language]["fertilizerComb"]+"2 :"}>
                 <View style={{flexDirection: "row", justifyContent: "space-around", width: "95%", marginTop: 15}}>
                     <View style={{alignItems: "center"}}>
-                        <Text>Nitrogen</Text>
+                        <Text>{transcription[lang.language]["nitrogen"]}</Text>
                         <Image source={require("../../assets/nitrogen.png")} style={{width: 50, height: 50, borderRadius: 5, marginTop: 5, marginBottom: 10}} />
                         <Text style={{color: "green",fontSize: 25}}>42.0%</Text>
                     </View>
                     <View style={{width: 0.5, height :"85%", backgroundColor: "green"}} />
                     <View style={{alignItems: "center"}}>
-                        <Text>Phosphorus</Text>
+                        <Text>{transcription[lang.language]["phosphorus"]}</Text>
                         <Image source={require("../../assets/phosphorus.png")} style={{width: 50, height: 50, borderRadius: 5, marginTop: 5, marginBottom: 10}} />
                         <Text style={{color: "green",fontSize: 25}}>33.0%</Text>
                     </View>
                     <View style={{width: 0.5, height :"85%", backgroundColor: "green"}} />
                     <View style={{alignItems: "center"}}>
-                        <Text>Potassium</Text>
+                        <Text>{transcription[lang.language]["potassium"]}</Text>
                         <Image source={require("../../assets/potassium.png")} style={{width: 50, height: 50, borderRadius: 5, marginTop: 5, marginBottom: 10}} />
                         <Text style={{color: "green",fontSize: 25}}>25.0%</Text>
                     </View>
                 </View>
             </Card>
             
-            <Card heading="Organic Manure :" image={<Image source={require("../../assets/fertilizer.png")} style={{width: 60, height: 60, borderRadius: 5, marginRight:"10%"}} />}>
+            <Card heading={transcription[lang.language]["organicManure"]} image={<Image source={require("../../assets/fertilizer.png")} style={{width: 60, height: 60, borderRadius: 5, marginRight:"10%"}} />}>
                 <Text style={{color: "#0e6e3a", fontSize: 30}}>39.55%</Text>
             </Card>
-            <Card heading="Bio Fertilizer :" image={<Image source={require("../../assets/compost.png")} style={{width: 60, height: 60, borderRadius: 5, marginRight:"10%"}} />}>
+            <Card heading={transcription[lang.language]["bioFertilizer"]} image={<Image source={require("../../assets/compost.png")} style={{width: 60, height: 60, borderRadius: 5, marginRight:"10%"}} />}>
                 <Text style={{color: "#0e6e3a", fontSize: 30}}>27.87%</Text>
             </Card>
         </ScrollView>
