67d014f38785e6d4578a070991b8cf23251f1d6b
Shivam Akhouri <shivamakhouri1142@gmail.com>
2022-06-17 11:06:56 +0530

    app.js changed

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default function App() {
           screenOptions={{
             headerShown: false,
           }}
-          initialRouteName="List"
+          initialRouteName="FarmerDashboard"
         >
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="RoutePage" component={Routepage} />
